7c797348a3fe71849e1720ce6ae4949150c2eb86
ssjy1919 <137855663@qq.com>
2026-08-09 03:31:11 +0800

    style: add spacing around transition hint

--- tests/i18n.test.ts
+++ tests/i18n.test.ts
@@ -26,6 +26,6 @@ describe('i18n', () => {
         expect(i18n.t('settings.alwaysWriteCreated.name')).toBe('Always write created time');
         expect(
             i18n.t('settings.states.transition', { next: 'x', nextName: 'Done' }),
-        ).toBe('( ◔ ω◔) ⇨         [x] Done');
+        ).toBe('   ( ◔ ω◔) ⇨   [x] Done');
     });
 });
